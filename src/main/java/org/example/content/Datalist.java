5cf4720389e1b66078f6b44db4d82c5475142d77
quangtinhle@gmail.com <Master01071986>
2020-03-15 23:23:21 +0100

    add Datalist

---
+++ src/main/java/org/example/content/Datalist.java
@@ -0,0 +1,33 @@
+package org.example.content;
+
+import com.google.gson.Gson;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.VerticalLayout;
+import org.example.entities.Candidate;
+import org.example.service.CandidateService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+public class Datalist extends VerticalLayout {
+    private CandidateService service = CandidateService.getInstance();
+    private Grid<Candidate> grid = new Grid<>(Candidate.class);
+    private CandidateForm candidateForm;
+
+    public Datalist() throws IOException {
+        candidateForm = new CandidateForm(this);
+        addComponents(grid, candidateForm);
+
+        grid.setColumns("fristName","lastName");
+        candidateForm.setCandidate(null);
+        grid.asSingleSelect().addValueChangeListener(e -> candidateForm.setCandidate(grid.asSingleSelect().getValue()));
+        updateList(service.findAll());
+    }
+
+    public void updateList(ArrayList<Candidate> candidates) {
+        System.out.println(new Gson().toJson(candidates));
+        grid.setItems(candidates);
+    }
+}
