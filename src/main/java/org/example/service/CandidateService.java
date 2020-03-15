f8ec383956ebf581c057cdb02b28307ff595375d
quangtinhle@gmail.com <Master01071986>
2020-03-15 23:35:54 +0100

    add CandidateService implements Serializable

---
+++ src/main/java/org/example/service/CandidateService.java
@@ -0,0 +1,65 @@
+package org.example.service;
+
+import com.google.gson.reflect.TypeToken;
+import org.example.entities.Candidate;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class CandidateService implements Serializable {
+
+    private static CandidateService instance;
+    private ArrayList<Candidate> candidates;
+
+    public static CandidateService getInstance() throws IOException {
+        if(instance == null) {
+            instance = new CandidateService();
+        }
+        return instance;
+    }
+    private CandidateService() throws IOException {
+        deserializing();
+    }
+
+    private void deserializing() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("src/main/resources/candidate.txt");
+        if(fileInputStream.available() == 0 ) {
+            this.candidates = new ArrayList<>();
+            return;
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        StringBuilder json = new StringBuilder();
+        String line = null;
+        while((line = bufferedReader.readLine())!= null)
+            json.append(line);
+        this.candidates = new Gson().fromJson(json.toString(),new TypeToken<ArrayList<Candidate>>(){}.getType());
+        bufferedReader.close();
+        inputStreamReader.close();
+        fileInputStream.close();
+    }
+
+    private void serializing() throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream("src/main/resources/candidate.txt");
+        BufferedOutputStream os = new BufferedOutputStream(fileOutputStream);
+        String json = new Gson().toJson(this.candidates);
+        os.write((json.getBytes()));
+        os.close();
+        fileOutputStream.close();
+    }
+
+    public void save(Candidate candidate) throws IOException {
+        candidates.add(candidate);
+        serializing();
+    }
+
+    public void delete(Candidate candidate) throws IOException {
+        candidates.remove(candidate.getId());
+        serializing();
+    }
+
+    public ArrayList<Candidate> findAll() {
+        return this.candidates;
+    }
+}
