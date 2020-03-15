7c941975ab1171dc80020cb28f3d6c257358cb4f
quangtinhle@gmail.com <Master01071986>
2020-03-15 20:45:13 +0100

    add Entities of Candidate

---
+++ src/main/java/org/example/entities/Candidate.java
@@ -0,0 +1,70 @@
+package org.example.entities;
+
+import java.time.LocalDate;
+
+public class Candidate {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private LocalDate birthDate;
+    private String email;
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
